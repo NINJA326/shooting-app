8348493861681d235de154e0b14aef44137ef833
NINJA326 <xxxmastsxxx@gmail.com>
2026-07-01 16:56:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'ninja-shooting-v11.3';
+const CACHE_VERSION = 'ninja-shooting-v11.5';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil((async()=>{
